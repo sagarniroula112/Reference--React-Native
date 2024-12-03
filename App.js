b4db9af3e989cfae5344b3f6bea6f889071489c5
Sagar Niroula <sagarniroula112@gmail.com>
2024-12-03 16:05:11 +0545

    Aligning items in a flexbox!

--- App.js
+++ App.js
@@ -8,13 +8,20 @@ export default function App() {
         backgroundColor: "#fff",
         flex: 1,
         flexDirection: "row",
+        justifyContent: "center", // align along horizontal axis (main axis)
+        alignItems: "center", // align along vertical axis (secondary axis)
       }}
     >
       <View
-        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 200,
+          alignSelf: "flex-start",
+        }}
       />
-      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "gold", width: 100, height: 200 }} />
+      <View style={{ backgroundColor: "tomato", width: 100, height: 200 }} />
     </View>
   );
 }
