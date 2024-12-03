cb1b321f99e8a15e000b14820a7a6a6f41054440
Sagar Niroula <sagarniroula112@gmail.com>
2024-12-03 15:52:47 +0545

    Flex introduction!

--- App.js
+++ App.js
@@ -1,36 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  Dimensions,
-  Button,
-  SafeAreaView,
-  StyleSheet,
-  Platform,
-  View,
-} from "react-native";
-
-import { useDeviceOrientation } from "@react-native-community/hooks";
+import React from "react";
+import { View } from "react-native";
 
 export default function App() {
-  // console.log(Dimensions.get("screen")); // Get dimensions of the device. Problem: Rotation won't update the dimensions.
-  const deviceOrientation = useDeviceOrientation();
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: "100%", // DIPs == Density Independent Pixels, Physical Pixels = DIPs x Scale Factor of device
-          height: deviceOrientation === "landscape" ? "100%" : "30%",
-        }}
-      ></View>
-    </SafeAreaView>
+    <View style={{ backgroundColor: "#fff", flex: 1 }}>
+      <View style={{ backgroundColor: "dodgerblue", flex: 2 }} />
+      <View style={{ backgroundColor: "gold", flex: 1 }} />
+      <View style={{ backgroundColor: "tomato", flex: 1 }} />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
---
+++ PreviousCodes/adjustWithOrientationDevice.js
@@ -0,0 +1,36 @@
+// import { StatusBar } from "expo-status-bar";
+// import {
+//   Dimensions,
+//   Button,
+//   SafeAreaView,
+//   StyleSheet,
+//   Platform,
+//   View,
+// } from "react-native";
+
+// import { useDeviceOrientation } from "@react-native-community/hooks";
+
+// export default function App() {
+//   // console.log(Dimensions.get("screen")); // Get dimensions of the device. Problem: Rotation won't update the dimensions.
+//   const deviceOrientation = useDeviceOrientation();
+
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <View
+//         style={{
+//           backgroundColor: "dodgerblue",
+//           width: "100%", // DIPs == Density Independent Pixels, Physical Pixels = DIPs x Scale Factor of device
+//           height: deviceOrientation === "landscape" ? "100%" : "30%",
+//         }}
+//       ></View>
+//     </SafeAreaView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+//   },
+// });
