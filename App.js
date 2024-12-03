a77aef766fb5843457e3dca8865d290f6a434737
Sagar Niroula <sagarniroula112@gmail.com>
2024-12-03 15:24:02 +0545

    Learned buttons and styles!

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, SafeAreaView, StyleSheet } from "react-native";
+import { Button, SafeAreaView, Alert, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={[styles.container, containerStyle]}>
       <Button
         color={"orange"}
         title="Click Me"
-        onPress={() => console.log("Button Tapped!")}
+        onPress={
+          () =>
+            // Alert.alert("My title", "My message", [
+            //   { text: "Yes", onPress: () => console.log("Yes") },
+            //   { text: "No", onPress: () => console.log("No") },
+            // ])
+            Alert.prompt("My title", "My message", (text) => console.log(text)) // doesn't work on android
+        }
       />
     </SafeAreaView>
   );
 }
+
+const containerStyle = { backgroundColor: "lightblue" };
 
 const styles = StyleSheet.create({
   container: {
