f88d5f2d04dc9293fe7f596a2dc86afce3077853
Sagar Niroula <sagarniroula112@gmail.com>
2024-12-03 16:11:08 +0545

    FlexWrap and AlignContent! Very Very Important!

--- App.js
+++ App.js
@@ -9,19 +9,24 @@ export default function App() {
         flex: 1,
         flexDirection: "row",
         justifyContent: "center", // align along horizontal axis (main axis)
-        alignItems: "center", // align along vertical axis (secondary axis)
+        alignItems: "center", // align along vertical axis (secondary axis) // alignment in one line
+        alignContent: "center", // determines alignment of entire content, only works with wrapping
+        flexWrap: "wrap",
       }}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
           width: 100,
-          height: 200,
-          alignSelf: "flex-start",
+          height: 100,
         }}
       />
-      <View style={{ backgroundColor: "gold", width: 100, height: 200 }} />
-      <View style={{ backgroundColor: "tomato", width: 100, height: 200 }} />
+      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "grey", width: 100, height: 100 }} />
+      <View
+        style={{ backgroundColor: "greenyellow", width: 100, height: 100 }}
+      />
     </View>
   );
 }
