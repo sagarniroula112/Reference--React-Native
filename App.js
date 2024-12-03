0b35f063f6bb7ea8031f4f27feed88c30d896e5f
Sagar Niroula <sagarniroula112@gmail.com>
2024-12-03 16:23:10 +0545

    Relative&Absolute positioning of items done!

--- App.js
+++ App.js
@@ -19,12 +19,21 @@ export default function App() {
           backgroundColor: "dodgerblue",
           // flexBasis: 100, // width or height
           // flexGrow: 1, // the view will grow to fill available space
-          flexShrink: 1, // this item can get shrunk in case of other items being overflown
-          width: 300,
+          // flexShrink: 1, // this item can get shrunk in case of other items being overflown
+          width: 100,
           height: 100,
         }}
       />
-      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
+      <View
+        style={{
+          top: 20, // they will work as by default, the items are position: relative, without changing layouts
+          left: 20,
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
+          position: "absolute",
+        }}
+      />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
     </View>
   );
